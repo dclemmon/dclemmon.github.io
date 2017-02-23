dda16ca108267ca3375eaa38398229f35245c957
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-23 14:18:35 -0700

    fixes typo

--- alert.js
+++ alert.js
@@ -4,7 +4,7 @@ var imgTags = document.getElementsByTagName("img");
 var searchText = "targetImg";
 var found;
 
-for (var i = 0; i < imgtags.length; i++) {
+for (var i = 0; i < imgTags.length; i++) {
     if (imgTags[i].getAttribute('alt') == searchText) {
         found = imgTags[i];
         break;
