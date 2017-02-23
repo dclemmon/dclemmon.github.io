e23914a1342aeb7852e29235b70d93b426f92d31
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-23 14:17:08 -0700

    Adds js to alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,14 @@
 alert('xss');
+
+var imgTags = document.getElementsByTagName("img");
+var searchText = "targetImg";
+var found;
+
+for (var i = 0; i < imgtags.length; i++) {
+    if (imgTags[i].getAttribute('alt') == searchText) {
+        found = imgTags[i];
+        break;
+    }
+}
+
+console.log(found);
