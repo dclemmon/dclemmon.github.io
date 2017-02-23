210442dc59cd6cc3079ad197fe7e0690e2d5ad9c
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-23 14:35:13 -0700

    adds finding parent div

--- alert.js
+++ alert.js
@@ -3,6 +3,7 @@ alert('xss');
 var imgTags = document.getElementsByTagName("img");
 var searchText = "targetImg";
 var found;
+var parentObj;
 
 for (var i = 0; i < imgTags.length; i++) {
     if (imgTags[i].getAttribute('alt') == searchText) {
@@ -11,4 +12,10 @@ for (var i = 0; i < imgTags.length; i++) {
     }
 }
 
+parentObj = found.parentNode;
+while(parentObj.getAttribute('name') != 'div') {
+    parentObj = parentObj.parentNode;
+}
+
 console.log(found);
+console.log(parentObj);
