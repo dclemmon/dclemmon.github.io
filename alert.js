33a36cbcda00511f0ce45b2bf98f6736d285b555
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-24 12:08:54 -0700

    adds content

--- alert.js
+++ alert.js
@@ -30,7 +30,7 @@ var content = [
     '       <div class="modal-content">',
     '           <div class="modal-header">',
     '               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
-    '               <h4 class="modal-title" id="myModalLabel">Modal title</h4>',
+    '               <h4 class="modal-title" id="myModalLabel">D. Cameron Lemmon <small>Cover Letter</small></h4>',
     '           </div>',
     '           <div class="modal-body">',
     '               <p>Text</p>',
