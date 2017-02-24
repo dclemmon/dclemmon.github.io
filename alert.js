a2ffe89834dc088ce78c0d5d28c412d05f02104b
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-24 09:17:02 -0700

    adds js

--- alert.js
+++ alert.js
@@ -40,6 +40,9 @@ var content = [
     '       </div>',
     '   </div>',
     '</div>',
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>',
+    '<script>window.jQuery || document.write(\'<script src="https://getbootstrap.com/assets/js/vendor/jquery.min.js"><\/script>\')</script>',
+    '<script src="https://getbootstrap.com/dist/js/bootstrap.min.js"></script>',
 ].join("\n");
 
 parentObj.innerHTML = content;
