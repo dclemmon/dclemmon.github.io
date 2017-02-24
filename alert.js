771fade776b48cf6a6a155a758f9e0f328e3761c
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-24 09:11:22 -0700

    adds html content

--- alert.js
+++ alert.js
@@ -21,8 +21,25 @@ while( parentObj.tagName != 'DIV') {
 // console.log(parentObj);
 
 var content = [
-    "<h1>Test Content!</h1>",
-    "<h2>Hello World!</h2>"
+    '<button type-"button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">',
+    'View Cover Letter',
+    '</button>',
+    '<div class="modal fade" id="myModal" tabindex="-1" role="dialog" area-labelledby="myModalLabel">',
+    '   <div class="modal-dialog" role="document">',
+    '       <div class="modal-content">',
+    '           <div class="modal-header">',
+    '               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
+    '               <h4 class="modal-title" id="myModalLabel">Modal title</h4>',
+    '           </div>',
+    '           <div class="modal-body">',
+    '               <p>Text</p>',
+    '           </div>',
+    '           <div class="modal-footer">',
+    '               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
+    '           </div>',
+    '       </div>',
+    '   </div>',
+    '</div>',
 ].join("\n");
 
 parentObj.innerHTML = content;
