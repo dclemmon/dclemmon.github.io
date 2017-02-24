a79f9c54a07266303600d52cd138588292b79561
Cameron Lemmon <david.cam.lemmon@hpe.com>
2017-02-24 09:23:04 -0700

    adds escaping

--- alert.js
+++ alert.js
@@ -42,7 +42,7 @@ var content = [
     '   </div>',
     '</div>',
     '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>',
-    '<script>window.jQuery || document.write(\'<script src="https://getbootstrap.com/assets/js/vendor/jquery.min.js"><\/script>\')</script>',
+    '<script>window.jQuery || document.write(\'<script src="https://getbootstrap.com/assets/js/vendor/jquery.min.js"><\\/script>\')</script>',
     '<script src="https://getbootstrap.com/dist/js/bootstrap.min.js"></script>',
 ].join("\n");
 
